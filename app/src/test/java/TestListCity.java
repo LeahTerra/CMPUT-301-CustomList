d89ec2f81d83a5bc7aaa3792deba3fe66d7163ef
Leah Sheptycki <lsheptyc@ualberta.ca>
2021-10-31 23:46:08 -0600

    Adding hasCity Test

--- app/src/test/java/TestListCity.java
+++ app/src/test/java/TestListCity.java
@@ -1,4 +1,5 @@
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 
 import com.example.simpleparadox.listycity.City;
 import com.example.simpleparadox.listycity.CustomList;
@@ -22,5 +23,10 @@ public class TestListCity {
         int listSize = list.getCount();
         list.addCity(new City("Halifax", "NS"));
         assertEquals(list.getCount(), listSize +1);
+    }
+
+    @Test
+    public void hasCityTest() {
+        assertTrue(list.hasCity(new City("Edmonton", "AB")));
     }
 }
