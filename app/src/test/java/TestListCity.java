69e687d6619cf6cd03eea83a305b102313928f71
Leah Sheptycki <lsheptyc@ualberta.ca>
2021-10-31 23:29:19 -0600

    add test 2

--- app/src/test/java/TestListCity.java
+++ app/src/test/java/TestListCity.java
@@ -1,2 +1,25 @@
-package PACKAGE_NAME;public class TestListCity {
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import com.example.simpleparadox.listycity.City;
+import com.example.simpleparadox.listycity.CustomList;
+
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import java.util.ArrayList;
+
+public class TestListCity {
+    private CustomList list;
+
+    @BeforeAll
+    public void createList() {
+        list = new CustomList(null, new ArrayList<City>());
+    }
+    @Test
+    public void addCityTest() {
+        int listSize = list.getCount();
+        list.addCity(new City("Halifax", "NS"));
+        assertEquals(list.getCount(), listSize +1);
+    }
 }
